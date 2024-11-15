b6e310d0a6f844eab2ca6381f36304b9f6ea7bad
mscrnt <trunksgt243@gmail.com>
2024-11-15 08:08:21 -0800

    Add Matomo tracking code to main.php

--- php/templates/main.php
+++ php/templates/main.php
@@ -7,7 +7,23 @@
     <link rel="stylesheet" href="/../static/css/all.min.css">
     <link rel="stylesheet" href="/../static/css/style.css">
     <script src="/../static/app.js" defer></script>
-</head>
+    <!-- Matomo -->
+    <script>
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        var u="//192.168.69.197:8343/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '2']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    })();
+    </script>
+    <!-- End Matomo Code -->
+
+    </head>
 <body>
     <div class="container mt-2">
         <h2>Hero Shard Calculator</h2>
